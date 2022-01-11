ad26df7392e22f105cfe45267047cd60a8542bd7
mariananbp <mariananbp@gmail.com>
2022-01-11 19:47:39 -0300

    lint

--- controllers/products.controllers.js
+++ controllers/products.controllers.js
@@ -1,8 +1,8 @@
-const { createProduct } = require('../services/productsService');
+const { createProduct, showAllProducts } = require('../services/productsService');
 const { created } = require('../utils/dictionary/statusCode');
-const { unprocessableEntity } = require('../utils/dictionary/statusCode');
+const { unprocessableEntity, success } = require('../utils/dictionary/statusCode');
 
-const productCreate = async (req, res, next) => {
+const productCreate = async (req, res, _next) => {
   try {
     const { name, quantity } = req.body;
     const newProduct = await createProduct(name, quantity);
@@ -12,6 +12,12 @@ const productCreate = async (req, res, next) => {
   }
 };
 
+const getAllProducts = async (req, res, _next) => {
+  const allProducts = await showAllProducts();
+  return res.status(success).json({ products: allProducts });
+};
+
 module.exports = {
   productCreate,
+  getAllProducts,
 };
