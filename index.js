5ad439e51cb2b50c19c834c3c2ebee71ace2c6cb
mariananbp <mariananbp@gmail.com>
2022-01-11 15:12:33 -0300

    Inicio do index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`Teste, escutando na porta ${PORT}`));
